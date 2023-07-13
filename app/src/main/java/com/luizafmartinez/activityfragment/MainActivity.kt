e89440c52c7e8aa5e21df6bb40afa8237297f972
alxmart <alxmartinez@gmail.com>
2023-07-13 19:03:02 -0300

    Testing activity lifecycle, creating messages in Logcat.

--- app/src/main/java/com/luizafmartinez/activityfragment/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/activityfragment/MainActivity.kt
@@ -3,14 +3,20 @@ package com.luizafmartinez.activityfragment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 class MainActivity : AppCompatActivity() {
+
     lateinit var buttonAbrir: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("ciclo_vida", "onCreate")   //<= Mensagem p/onCreate
+
         setContentView(R.layout.activity_main)
 
         buttonAbrir = findViewById(R.id.button_abrir)
+
         buttonAbrir.setOnClickListener {
 
             val intent = Intent(this,
@@ -19,4 +25,42 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    override  fun onStart() {
+        super.onStart()
+        //Simula -> carregar dados
+        Log.i("ciclo_Vida", "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo_Vida", "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo_Vida", "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo_Vida", "onStop")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("ciclo_Vida", "onRestart")
+    }
+    override fun onDestroy() {
+        Log.i("ciclo_Vida", "onDestroy")
+        super.onDestroy()
+        Log.i("ciclo_Vida", "onDestroy")
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
