39377ef8801b0727daf5d8701236bc7c0c50c784
alxmart <alxmartinez@gmail.com>
2023-07-13 15:21:03 -0300

    Created intent and finish to open and close DetalhesActivity.

--- app/src/main/java/com/luizafmartinez/activityfragment/DetalhesActivity.kt
+++ app/src/main/java/com/luizafmartinez/activityfragment/DetalhesActivity.kt
@@ -2,10 +2,33 @@ package com.luizafmartinez.activityfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var buttonFechar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        buttonFechar = findViewById(R.id.button_fechar)
+        buttonFechar.setOnClickListener {
+            finish()
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/luizafmartinez/activityfragment/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/activityfragment/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.luizafmartinez.activityfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonAbrir: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAbrir = findViewById(R.id.button_abrir)
+        buttonAbrir.setOnClickListener {
+
+            val intent = Intent(this,
+                DetalhesActivity::class.java)
+            // Inicia nova tela
+            startActivity(intent)
+        }
     }
 }
