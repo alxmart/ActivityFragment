e2eb2a8975780c4c045744ac433606ef2697d353
alxmart <alxmartinez@gmail.com>
2023-07-23 14:42:13 -0300

    Created data class Filme.

---
+++ app/src/main/java/com/luizafmartinez/activityfragment/Filme.kt
@@ -0,0 +1,9 @@
+package com.luizafmartinez.activityfragment
+
+data class Filme(
+    val nome: String,
+    val descricao: String,
+    val avaliacoes: Double,
+    val diretor: String,
+    val distribuidor: String
+)
